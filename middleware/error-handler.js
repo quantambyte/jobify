7b965baef34e2c7dc6c22260be5c301bd8fb1e73
Uzair Asif <uzairasif729@gmail.com>
2022-02-16 16:55:09 +0500

    Update error-handler.js

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,20 +1,12 @@
 import { StatusCodes } from 'http-status-codes';
 
 // error handler takes 4 parameters
-const errorHandlerMiddleware = (
-  err,
-  req,
-  res,
-  next
-) => {
+const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err.message);
   const defaultError = {
     statusCode:
-      err.statusCode ||
-      StatusCodes.INTERNAL_SERVER_ERROR,
-    msg:
-      err.message ||
-      'Something went wrong, try again later',
+      err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong, try again later',
   };
 
   // if any field is missing
